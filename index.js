b3faa5d55e6a56df30a5e24542a18fa8a4a2f189
Patricio <patricioprp06@gmail.com>
2019-04-09 19:30:28 -0300

    hasta :HTT Request with React Native

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
 //Importar la libreria para crear un componente
-import React from 'react';
-import {Text,AppRegistry} from 'react-native';
-import Header from './src/components/Header';
+import React from "react";
+import { View, AppRegistry } from "react-native";
+import Header from "./src/components/Header";
+import AlbumList from "./src/components/AlbumList";
 
 //Crear el Componente
 
-const App = () =>(
-       <Header headerText={'Albums!'}/>
-   );
+const App = () => (
+  <View>
+    <Header headerText={"Albums!"} />
+    <AlbumList />
+  </View>
+);
 //Renderizar en el Dispositivo
 
-AppRegistry.registerComponent('albums',() =>App);
+AppRegistry.registerComponent("albums", () => App);
